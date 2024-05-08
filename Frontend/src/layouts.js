63632dbefda4cc0c4d465914ff13adee99b68900
akinbola247 <akinbolakehinde247@gmail.com>
2024-05-08 20:58:32 +0100

    changes

--- Frontend/src/layouts.js
+++ Frontend/src/layouts.js
@@ -1,72 +1,72 @@
-const layouts = {
-    dashboard_k: {
-        lg: [
-            {i: 'stat-cause', x: 0, y: 0, w: 1, h: 1},
-            {i: 'stat-teeth', x: 1, y: 0, w: 1, h: 1},
-            {i: 'stat-heart', x: 2, y: 0, w: 1, h: 1},
-            {i: 'payments-history', x: 0, y: 1, w: 1, h: 3},
-            {i: 'recent-tests', x: 1, y: 1, w: 2, h: 3}
-        ],
-        md: [
-            {i: 'stat-cause', x: 1, y: 0, w: 1, h: 1},
-            {i: 'stat-teeth', x: 1, y: 1, w: 1, h: 1},
-            {i: 'stat-heart', x: 1, y: 2, w: 1, h: 1},
-            {i: 'payments-history', x: 0, y: 0, w: 1, h: 3},
-            {i: 'recent-tests', x: 0, y: 3, w: 2, h: 3}
-        ]
-    },
-    finances: {
-        lg: [
-            {i: 'balance', x: 0, y: 0, w: 1, h: 1},
-            {i: 'payments-feed', x: 1, y: 0, w: 2, h: 4},
-            {i: 'credit-cards', x: 0, y: 1, w: 1, h: 3},
-        ],
-        md: [
-            {i: 'balance', x: 0, y: 0, w: 1, h: 1},
-            {i: 'payments-feed', x: 1, y: 0, w: 1, h: 4},
-            {i: 'credit-cards', x: 0, y: 1, w: 1, h: 3},
-        ],
-    },
-    doctor_reviews: {
-        lg: [
-            {i: 'doctors-rating-list', x: 2, y: 0, w: 1, h: 4},
-            {i: 'doctors-reviews-content', x: 0, y: 0, w: 2, h: 4},
-        ],
-        md: [
-            {i: 'doctors-rating-list', x: 0, y: 0, w: 2, h: 4},
-            {i: 'doctors-reviews-content', x: 0, y: 0, w: 0, h: 0, minW: 0, minH: 0},
-        ]
-    },
-    patient_reviews: {
-        lg: [
-            {i: 'doctors-rating-list', x: 0, y: 0, w: 1, h: 4},
-            {i: 'patient-reviews-content', x: 1, y: 0, w: 2, h: 4},
-        ],
-        md: [
-            {i: 'doctors-rating-list', x: 0, y: 0, w: 2, h: 4},
-            {i: 'patient-reviews-content', x: 0, y: 0, w: 0, h: 0, minW: 0, minH: 0},
-        ]
-    },
-    doctor_messenger: {
-        lg: [
-            {i: 'contacts-list', x: 0, y: 0, w: 1, h: 4},
-            {i: 'messenger', x: 1, y: 0, w: 2, h: 4},
-        ],
-        md: [
-            {i: 'contacts-list', x: 0, y: 0, w: 2, h: 4},
-            {i: 'messenger', x: 0, y: 0, w: 0, h: 0, minW: 0, minH: 0},
-        ]
-    },
-    patient_messenger: {
-        lg: [
-            {i: 'contacts-list', x: 0, y: 0, w: 1, h: 4},
-            {i: 'messenger', x: 1, y: 0, w: 2, h: 4},
-        ],
-        md: [
-            {i: 'contacts-list', x: 0, y: 0, w: 2, h: 4},
-            {i: 'messenger', x: 0, y: 0, w: 0, h: 0, minW: 0, minH: 0},
-        ]
-    }
-};
+// const layouts = {
+//     dashboard_k: {
+//         lg: [
+//             {i: 'stat-cause', x: 0, y: 0, w: 1, h: 1},
+//             {i: 'stat-teeth', x: 1, y: 0, w: 1, h: 1},
+//             {i: 'stat-heart', x: 2, y: 0, w: 1, h: 1},
+//             {i: 'payments-history', x: 0, y: 1, w: 1, h: 3},
+//             {i: 'recent-tests', x: 1, y: 1, w: 2, h: 3}
+//         ],
+//         md: [
+//             {i: 'stat-cause', x: 1, y: 0, w: 1, h: 1},
+//             {i: 'stat-teeth', x: 1, y: 1, w: 1, h: 1},
+//             {i: 'stat-heart', x: 1, y: 2, w: 1, h: 1},
+//             {i: 'payments-history', x: 0, y: 0, w: 1, h: 3},
+//             {i: 'recent-tests', x: 0, y: 3, w: 2, h: 3}
+//         ]
+//     },
+//     finances: {
+//         lg: [
+//             {i: 'balance', x: 0, y: 0, w: 1, h: 1},
+//             {i: 'payments-feed', x: 1, y: 0, w: 2, h: 4},
+//             {i: 'credit-cards', x: 0, y: 1, w: 1, h: 3},
+//         ],
+//         md: [
+//             {i: 'balance', x: 0, y: 0, w: 1, h: 1},
+//             {i: 'payments-feed', x: 1, y: 0, w: 1, h: 4},
+//             {i: 'credit-cards', x: 0, y: 1, w: 1, h: 3},
+//         ],
+//     },
+//     doctor_reviews: {
+//         lg: [
+//             {i: 'doctors-rating-list', x: 2, y: 0, w: 1, h: 4},
+//             {i: 'doctors-reviews-content', x: 0, y: 0, w: 2, h: 4},
+//         ],
+//         md: [
+//             {i: 'doctors-rating-list', x: 0, y: 0, w: 2, h: 4},
+//             {i: 'doctors-reviews-content', x: 0, y: 0, w: 0, h: 0, minW: 0, minH: 0},
+//         ]
+//     },
+//     patient_reviews: {
+//         lg: [
+//             {i: 'doctors-rating-list', x: 0, y: 0, w: 1, h: 4},
+//             {i: 'patient-reviews-content', x: 1, y: 0, w: 2, h: 4},
+//         ],
+//         md: [
+//             {i: 'doctors-rating-list', x: 0, y: 0, w: 2, h: 4},
+//             {i: 'patient-reviews-content', x: 0, y: 0, w: 0, h: 0, minW: 0, minH: 0},
+//         ]
+//     },
+//     doctor_messenger: {
+//         lg: [
+//             {i: 'contacts-list', x: 0, y: 0, w: 1, h: 4},
+//             {i: 'messenger', x: 1, y: 0, w: 2, h: 4},
+//         ],
+//         md: [
+//             {i: 'contacts-list', x: 0, y: 0, w: 2, h: 4},
+//             {i: 'messenger', x: 0, y: 0, w: 0, h: 0, minW: 0, minH: 0},
+//         ]
+//     },
+//     patient_messenger: {
+//         lg: [
+//             {i: 'contacts-list', x: 0, y: 0, w: 1, h: 4},
+//             {i: 'messenger', x: 1, y: 0, w: 2, h: 4},
+//         ],
+//         md: [
+//             {i: 'contacts-list', x: 0, y: 0, w: 2, h: 4},
+//             {i: 'messenger', x: 0, y: 0, w: 0, h: 0, minW: 0, minH: 0},
+//         ]
+//     }
+// };
 
-export default layouts;
+// export default layouts;
